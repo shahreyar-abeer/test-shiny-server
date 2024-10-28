59d40096fb08efccac396e3776ee0acdde5d5134
shahreyar-abeer <shahreyar.abeer@gmail.com>
2024-10-28 09:54:09 +0600

    first commit

---
+++ app.R
@@ -0,0 +1,15 @@
+
+library(shiny)
+library(jsonlite)
+
+ui = fluidPage(
+  numericInput("n", "n", 1),
+  plotOutput("plot")
+)
+
+
+server = function(input, output) {
+  output$plot <- renderPlot( plot(head(cars, input$n)) )
+}
+
+shinyApp(ui = ui, server = server)
